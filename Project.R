e112e242e2ae18b4c7e3447f1348626fc5608516
StefaniaBianco <146854140+StefaniaBianco@users.noreply.github.com>
2024-02-07 18:52:03 +0100

    Update Project.R

--- Project.R
+++ Project.R
@@ -66,6 +66,56 @@ mean(Nord$`Pedestrian areas`)
 mean(Centro$`Pedestrian areas`)
 mean(Mezzogiorno$`Pedestrian areas`)
 
+
+#mean of variable "cycling lanes"
+mean_cycling_lanes<-mean(newdf$`Cycling lanes`)
+mean_cycling_lanes_nord<-mean(Nord$`Cycling lanes`)
+mean_cycling_lanes_centro<-mean(Centro$`Cycling lanes`)
+mean_cycling_lanes_mezzogiorno<-mean(Mezzogiorno$`Cycling lanes`)
+
+#variance of variable "cycling lanes"// var=M(x^2)-M(x)^2
+variance_cycling_lanes<-mean(newdf$`Cycling lanes`^2)-mean(newdf$`Cycling lanes`)^2
+variance_cycling_lanes_nord<-mean(Nord$`Cycling lanes`^2)-mean(Nord$`Cycling lanes`)^2
+variance_cycling_lanes_centro<-mean(Centro$`Cycling lanes`^2)-mean(Centro$`Cycling lanes`)^2
+variance_cycling_lanes_mezzogiorno<-mean(Mezzogiorno$`Cycling lanes`^2)-mean(Mezzogiorno$`Cycling lanes`)^2
+
+#standard deviation of variable "cycling lanes" // sd=sqrt(var)
+sd_cycling_lanes<-sqrt(variance_cycling_lanes)
+sd_cycling_lanes_nord<-sqrt(variance_cycling_lanes_nord)
+sd_cycling_lanes_centro<-sqrt(variance_cycling_lanes_centro)
+sd_cycling_lanes_mezzogiorno<-sqrt(variance_cycling_lanes_mezzogiorno)
+
+#coefficient of variation of variable "cycling lanes" // cv=sd/mean
+cv_cycling_lanes<-sd_cycling_lanes/mean_cycling_lanes
+cv_cycling_lanes_nord<-sd_cycling_lanes_nord/mean_cycling_lanes_nord
+cv_cycling_lanes_centro<-sd_cycling_lanes_centro/mean_cycling_lanes_centro
+cv_cycling_lanes_mezzogiorno<-sd_cycling_lanes_mezzogiorno/mean_cycling_lanes_mezzogiorno
+
+#mean of variable "motorization rate"
+mean_motorization_rate<-mean(newdf$`Motorization rate`)
+mean_motorization_rate_nord<-mean(Nord$`Motorization rate`)
+mean_motorization_rate_centro<-mean(Centro$`Motorization rate`)
+mean_motorization_rate_mezzogiorno<-mean(Mezzogiorno$`Motorization rate`)
+
+#variance of variable "Motorization rate"// var=M(x^2)-M(x)^2
+variance_motorization_rate<-mean(newdf$`Motorization rate`^2)-mean(newdf$`Motorization rate`)^2
+variance_motorization_rate_nord<-mean(Nord$`Motorization rate`^2)-mean(Nord$`Motorization rate`)^2
+variance_motorization_rate_centro<-mean(Centro$`Motorization rate`^2)-mean(Centro$`Motorization rate`)^2
+variance_motorization_rate_mezzogiorno<-mean(Mezzogiorno$`Motorization rate`^2)-mean(Mezzogiorno$`Motorization rate`)^2
+
+#standard deviation of variable "Motorization rate" // sd=sqrt(var)
+sd_motorization_rate<-sqrt(variance_motorization_rate)
+sd_motorization_rate_nord<-sqrt(variance_motorization_rate_nord)
+sd_motorization_rate_centro<-sqrt(variance_motorization_rate_centro)
+sd_motorization_rate_mezzogiorno<-sqrt(variance_motorization_rate_mezzogiorno)
+
+#coefficient of variation of variable "Motorization rate" // cv=sd/mean
+cv_motorization_rate<-sd_motorization_rate/mean_motorization_rate
+cv_motorization_rate_nord<-sd_motorization_rate/mean_motorization_rate_nord
+cv_motorization_rate_centro<-sd_motorization_rate/mean_motorization_rate_centro
+cv_motorization_rate_mezzogiorno<-sd_motorization_rate/mean_motorization_rate_mezzogiorno
+
+
 mean(Nord$`Urban ecosystem`)
 mean(Centro$`Urban ecosystem`)
 mean(Mezzogiorno$`Urban ecosystem`)
